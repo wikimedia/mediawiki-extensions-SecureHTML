0881e3f2ff25086e7059760655bee7de1662b40f
raymond <raimond.spekking@gmail.com>
2013-02-25 12:40:06 +0100

    Consistency tweaks in preparation for adding extension to translatewiki.net
    * Remove Title Case
    * Spinkle some <code></code>
    * Kill double spaces and use line breaks for new sentences instead
    
    Change-Id: Ia5d9c85db6e84f32fc8e20a4332b480750bbe480

--- SecureHTML.i18n.php
+++ SecureHTML.i18n.php
@@ -11,12 +11,13 @@ $messages = array();
 $messages['en'] = array(
 	'securehtml' => 'Secure HTML',
 	'securehtml-desc' => 'Lets you include arbitrary HTML in an authorized and secure way',
-	'securehtml-nokeys' => 'Error: wgSecureHTMLSecrets is not populated',
-	'securehtml-legacykeys' => 'Warning: wgSecureHTMLSecrets (Version 2) is not populated, but shtml_keys (Version 1) is.  Please convert to Version 2 hashes as soon as possible, as Version 1 is deprecated.',
-	'securehtml-invalidhash' => 'Error: Invalid hash',
-	'securehtml-invalidversion' => 'Error: Invalid version',
-	'securehtml-input-title' => 'HTML Input',
-	'securehtml-generatedoutput-title' => 'Generated Output',
+	'securehtml-nokeys' => 'Error: <code>$wgSecureHTMLSecrets</code> is not populated.',
+	'securehtml-legacykeys' => 'Warning: <code>$wgSecureHTMLSecrets</code> (version 2) is not populated, but <code>$shtml_keys</code> (version 1) is.
+Please convert to version 2 hashes as soon as possible, as version 1 is deprecated.',
+	'securehtml-invalidhash' => 'Error: Invalid hash.',
+	'securehtml-invalidversion' => 'Error: Invalid version.',
+	'securehtml-input-title' => 'HTML input',
+	'securehtml-generatedoutput-title' => 'Generated output',
 	'securehtml-renderedhhtml-title' => 'Rendered HTML',
 	'securehtml-form-version' => 'Hash version',
 	'securehtml-form-deprecated' => 'deprecated',
@@ -24,8 +25,10 @@ $messages['en'] = array(
 	'securehtml-form-keysecret' => 'Key secret',
 	'securehtml-form-html' => 'Raw HTML',
 	'securehtml-form-submit' => 'Submit',
-	'securehtml-inputinstructions' => 'Enter a key name (optional), key secret, and desired raw HTML.  If no key name is specified, the first key in wgSecureHTMLSecrets will be assumed.',
-	'securehtml-outputinstructions' => 'Copy the code above EXACTLY and paste it into the wiki editor.  If the generated code does not work, try removing all linefeeds from the input HTML and re-generate.',
+	'securehtml-inputinstructions' => 'Enter a key name (optional), key secret, and desired raw HTML.
+If no key name is specified, the first key in <code>$wgSecureHTMLSecrets</code> will be assumed.',
+	'securehtml-outputinstructions' => 'Copy the code above EXACTLY and paste it into the wiki editor.
+If the generated code does not work, try removing all linefeeds from the input HTML and re-generate.',
 );
 
 /** Message documentation (Message documentation) */
