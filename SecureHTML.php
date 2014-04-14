73ef65968265369b199099ef2afc01915e01f366
Ryan Finnie <ryan@finnie.org>
2014-04-14 00:23:40 -0700

    Version 2.4.1
    Change-Id: I886d60b0c339c3befcdff6520f61c7387c208077

--- SecureHTML.php
+++ SecureHTML.php
@@ -39,7 +39,7 @@ $wgExtensionCredits['parserhook'][] = $wgExtensionCredits['specialpage'][] = arr
 	'author' => 'Ryan Finnie',
 	'url' => 'https://www.mediawiki.org/wiki/Extension:Secure_HTML',
 	'descriptionmsg' => 'securehtml-desc',
-	'version' => '2.3.0',
+	'version' => '2.4.1',
 );
 
 # Default configuration globals
